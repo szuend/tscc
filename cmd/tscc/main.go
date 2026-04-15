566b6bcd0d7501346e56e95d3a0059d907981980
Simon Zünd <simon.zuend@gmail.com>
2026-04-15 20:39:17 +0200

    feat: add --always-strict flag and boolean handling

--- cmd/tscc/main.go
+++ cmd/tscc/main.go
@@ -19,12 +19,35 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/spf13/pflag"
 )
 
 func main() {
-	pflag.Parse()
+	flags := pflag.NewFlagSet("tscc", pflag.ExitOnError)
+
+	var alwaysStrict bool
+
+	flags.BoolVar(&alwaysStrict, "always-strict", true, "Files are parsed in ECMAScript strict mode, and emit \"use strict\" (--no-always-strict to disable)")
+
+	args := make([]string, len(os.Args))
+	copy(args, os.Args)
+
+	// Rewrite negated boolean flags e.g. --no-always-strict to --always-strict=false
+	for i, arg := range args {
+		if !strings.HasPrefix(arg, "--no-") || strings.Contains(arg, "=") {
+			continue
+		}
+
+		potentialFlagName := strings.TrimPrefix(arg, "--no-")
+		flag := flags.Lookup(potentialFlagName)
+		if flag != nil && flag.Value.Type() == "bool" {
+			args[i] = fmt.Sprintf("--%s=false", potentialFlagName)
+		}
+	}
+
+	_ = flags.Parse(args[1:])
 	fmt.Fprintln(os.Stderr, "tscc: not yet implemented")
 	os.Exit(1)
 }
